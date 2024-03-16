b6279c81b25b463383be02fe42e3a04813543629
沙滩上的鱼 <3516464843@qq.com>
2024-03-17 00:01:05 +0800

    feat: build the new project

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,170 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import { useAuthGuard } from "./guard";
 
-const routes: readonly RouteRecordRaw[] = [];
+const routes: readonly RouteRecordRaw[] = [
+  {
+    path: '',
+    name: 'layout',
+    component: () => import('@/components/Layout/index.vue'),
+    children: [
+      {
+        path: '/center',
+        name: 'user-center',
+        meta: { key: 'user-center', title: '个人中心', to: '/center' },
+        component: () => import('@/views/common/center/index.vue'),
+      },
+      {
+        path: '/home',
+        name: 'user-home',
+        meta: { key: 'user-home', title: '主页', to: '/home' },
+        component: () => import('@/views/common/home/index.vue'),
+      },
+      {
+        path: '/club',
+        name: 'club',
+        meta: { key: 'club', relation: '1' },
+        children: [
+          {
+            path: 'list',
+            name: 'club-list',
+            meta: { key: 'club-list', relation: '11', title: '社团列表', to: '/club/list' },
+            component: () => import('@/views/club/list.vue'),
+          },
+          {
+            path: 'detail/:id',
+            name: 'club-detail',
+            meta: { key: 'club-detail', relation: '111', title: '社团详情', to: '' },
+            component: () => import('@/views/club/detail.vue'),
+          },
+          {
+            path: 'apply',
+            name: 'club-apply',
+            meta: { key: 'club-apply', relation: '112', title: '社团申请', to: '/club/apply'},
+            component: () => import('@/views/club/apply.vue'),
+          },
+        ],
+      },
+      {
+        path: '/activity',
+        name: 'activity',
+        meta: { key: 'activity', relation: '2' },
+        children: [
+          {
+            path: 'list',
+            name: 'activity-list',
+            meta: { key: 'activity-list', relation: '21', title: '活动列表', to: '/activity/list' },
+            component: () => import('@/views/activity/list.vue'),
+          },
+          {
+            path: 'detail/:id',
+            name: 'activity-detail',
+            meta: { key: 'activity-detail', relation: '211', title: '活动详情', to: '' },
+            component: () => import('@/views/activity/detail.vue'),
+          },
+          {
+            path: 'apply',
+            name: 'activity-apply',
+            meta: { key: 'activity-apply', relation: '212', title: '活动申请', to: '/activity/apply' },
+            component: () => import('@/views/activity/apply.vue'),
+          },
+        ],
+      },
+      {
+        path: '/fund',
+        name: 'fund',
+        meta: { key: 'fund', relation: '3' },
+        children: [
+          {
+            path: 'list',
+            name: 'fund-list',
+            meta: { key: 'fund-list', relation: '31', title: '经费列表', to: '/fund/list' },
+            component: () => import('@/views/fund/list.vue'),
+          },
+          {
+            path: 'detail/:id',
+            name: 'fund-detail',
+            meta: { key: 'fund-detail', relation: '311', title: '经费详情', to: '' },
+            component: () => import('@/views/fund/detail.vue'),
+          },
+          {
+            path: 'apply',
+            name: 'fund-apply',
+            meta: { key: 'fund-apply', relation: '312', title: '经费申请', to: '/fund/apply' },
+            component: () => import('@/views/fund/apply.vue'),
+          },
+        ],
+      },
+      {
+        path: '/role',
+        name: 'role',
+        meta: { key: 'role', relation: '4' },
+        children: [
+          {
+            path: 'list',
+            name: 'role-list',
+            meta: { key: 'role-list', relation: '41', title: '分组列表', to: '/role/list' },
+            component: () => import('@/views/role/list.vue'),
+          },
+        ],
+      },
+      {
+        path: '/user',
+        name: 'user',
+        meta: { key: 'user', relation: '5' },
+        children: [
+          {
+            path: 'list',
+            name: 'user-list',
+            meta: { key: 'user-list', relation: '51', title: '用户列表', to: '/user/list' },
+            component: () => import('@/views/user/list.vue'),
+          },
+        ],
+      },
+      {
+        path: '/log',
+        name: 'log',
+        meta: { key: 'log', relation: '6' },
+        children: [
+          {
+            path: 'list',
+            name: 'log-list',
+            meta: { key: 'log-list', relation: '61', title: '日志列表', to: '/log/list' },
+            component: () => import('@/views/log/list.vue'),
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: '/login',
+    name: 'login',
+    meta: { title: '登录' },
+    component: () => import('@/views/common/login/index.vue'),
+  },
+  {
+    path: '/exception',
+    name: 'exception',
+    children: [
+      {
+        path: '/403',
+        name: 'Forbidden',
+        meta: { title: '权限不足' },
+        component: () => import('@/views/common/exception/403.vue'),
+      },
+      {
+        path: '/404',
+        name: 'NotFound',
+        meta: { title: '页面丢失' },
+        component: () => import('@/views/common/exception/404.vue'),
+      },
+    ],
+  },
+  {
+    path: '/:pathMatch(.*)',
+    name: 'non-exist',
+    redirect: 'exception/404',
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
