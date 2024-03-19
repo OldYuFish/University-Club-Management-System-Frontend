8294b386ee42cdcd9dc4935f60c51d9e7eac8b9c
沙滩上的鱼 <3516464843@qq.com>
2024-03-19 19:26:15 +0800

    feat: build the new project

---
+++ src/components/Crumb/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <ElBreadcrumb separator="/" class="pb-5">
+    <ElBreadcrumbItem v-for="crumb of store.state.common.crumbs" :to="crumb.to">
+      {{ crumb.title }}
+    </ElBreadcrumbItem>
+  </ElBreadcrumb>
+</template>
+<script lang="ts" setup>
+import store from "@/store";
+</script>
--- src/components/Layout/index.vue
+++ src/components/Layout/index.vue
@@ -1,6 +1,54 @@
 <template>
-
+  <UseWindowSize v-slot="{ height }">
+    <ElConfigProvider :locale="zh">
+      <ElContainer :style="{ height: `${height}px` }">
+        <ElHeader class="box-content p-0 h-16 py-1 header">
+          <OFHeader />
+        </ElHeader>
+        <ElContainer>
+          <ElAside class="relative w-auto">
+            <OFAside :height="height-72" />
+          </ElAside>
+          <ElScrollbar :style="{ height: `${height-72}px` }" class="w-full">
+            <ElMain>
+              <OFCrumb />
+              <RouterView v-slot="{ Component, route }">
+                <Transition
+                  v-if="route.meta.keepAlive"
+                  :name="route.meta.transition.toString() || 'fade'"
+                  mode="out-in"
+                  appear
+                >
+                  <KeepAlive :max="5">
+                    <Component :is="Component" :key="route.meta.key || route.name" />
+                  </KeepAlive>
+                </Transition>
+                <Transition
+                  v-if="!route.meta.keepAlive"
+                  :name="route.meta.transition.toString() || 'fade'"
+                  mode="out-in"
+                  appear
+                >
+                  <Component :is="Component" :key="route.meta.key || route.name" />
+                </Transition>
+              </RouterView>
+            </ElMain>
+          </ElScrollbar>
+        </ElContainer>
+      </ElContainer>
+    </ElConfigProvider>
+  </UseWindowSize>
 </template>
-
 <script lang="ts" setup>
+import { UseWindowSize } from "@vueuse/components";
+import zh from "element-plus/es/locale/lang/zh-cn";
+import OFHeader from "./Header.vue";
+import OFAside from "./Aside.vue";
+import OFCrumb from "../Crumb/index.vue";
 </script>
+<style scoped>
+.header {
+  box-shadow: 0 3px 10px rgba(0, 0, 0, 0.05);
+  z-index: 1;
+}
+</style>
