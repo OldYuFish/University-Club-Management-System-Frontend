e823fd845d516226ba0f980f0c834c1dd44290af
OldYuFish <97739023+OldYuFish@users.noreply.github.com>
2024-08-23 18:32:16 +0800

    Delete dist directory

--- dist/assets/raf-DDUMWbXU.js
+++
@@ -1,1 +0,0 @@
-import{aC as o}from"./index-CoVBmhUR.js";const e=i=>o?window.requestAnimationFrame(i):setTimeout(i,16),a=i=>o?window.cancelAnimationFrame(i):clearTimeout(i);export{a as c,e as r};
--- dist/assets/strings-C9rVxj0s.js
+++
@@ -1,1 +0,0 @@
-import{cs as a}from"./index-CoVBmhUR.js";const p=(e="")=>e.replace(/[|\\{}()[\]^$+*?.]/g,"\\$&").replace(/-/g,"\\x2d"),s=e=>a(e);export{s as c,p as e};
